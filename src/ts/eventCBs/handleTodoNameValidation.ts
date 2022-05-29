9de555ee56551870ffdcb196c57fa665a2361b50
Athma Chidambaravasi <athma.vasi@protonmail.com>
2022-05-29 10:36:15 -0600

    add comment in handleTodoNameValidation

--- src/ts/eventCBs/handleTodoNameValidation.ts
+++ src/ts/eventCBs/handleTodoNameValidation.ts
@@ -1,4 +1,5 @@
 const handleTodoNameValidation = function (this: HTMLInputElement, ev: InputEvent) {
+	//to avoid input registering as invalid, if a custom validity was previously set, when input contains valid value upon submission
 	this.setCustomValidity('')
 	this.checkValidity()
 }
