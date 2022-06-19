2ee15d57a52f032b7fa53801ce9853868773f671
Athma Chidambaravasi <athma.vasi@protonmail.com>
2022-06-19 08:47:26 -0600

    adjust project modal absolute position to center, mode to production

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
 		path: path.resolve(__dirname, 'dist'),
 		publicPath: 'auto',
 	},
-	mode: 'development',
+	mode: 'production',
 	plugins: [new MiniCssExtractPlugin()],
 	module: {
 		rules: [
